bfa18ecf33ac174b694d0288a42c6acbc9eb11dd
supercaliflipette <cverein.professionnel@yahoo.fr>
2023-06-10 22:31:01 +0200

    feat(init): fix import not found

--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -1,36 +1,28 @@
 import {Layout} from '../../components/Layout'
-import { getPlanningByID, getAllPlannings } from "../api/project"
-import {IPlanning} from '@/interfaces/index'
-import {PlanningDay} from '@/components/plannings/PlanningDay'
-import {Bandeau} from '@/components/Bandeau'
+
 
 type PlanningProps = {
-    planning: IPlanning,
+    planning: string,
     id: string
 }
 export default function PlanningsPage({planning, id}:PlanningProps) {
     return (<Layout title="Clementinestla planning" metaName = "Planning" metaDescription="Planning, horaires et disponibilité des differents creneaux horaires à l'atelier de Clementinestla">
-        <Bandeau txt={planning.title}/>        
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
-            {planning.days.map((day, index) => 
-             <PlanningDay  key={index} day={day} id={id} dayNumber={index+1}/>
-             )}
-        </div>
+
     </Layout>)
 }
 
-export async function getStaticProps(context:any){
+/*export async function getStaticProps(context:any){
     const planning = getPlanningByID(context.params.id)
     const id = context.params.id
     return {
         props: {
             planning,
             id
-        } 
+        }
     }
-}
+}*/
 
-export async function getStaticPaths(){
+/*export async function getStaticPaths(){
     let paths = await getAllPlannings()
     const paramsPaths = paths.map(planning => ({
         params: { id:planning.id, title:planning.title }
@@ -39,4 +31,4 @@ export async function getStaticPaths(){
         paths: paramsPaths,
         fallback: false
     }
-}
+}*/
