41484ee9e678e7b81d3ca40302d3b2e474b49e17
IPreferWater <ipreferwater2@gmail.com>
2023-07-22 21:08:41 +0200

    feat(init): fixe the title of page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -19,15 +19,15 @@ export const Layout = ({ children, title, metaName, metaDescription, projects}: 
   return <div>
     <Head>
       <title>{title}</title>
-      <meta charSet="utf-8" />      
+      <meta charSet="utf-8" />
       <link rel="shortcut icon" href="favicon.gif" />
     </Head>
     <header className='mb-4'>
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     <meta name={metaName} content={metaDescription}/>
     <div className='flex flex-row justify-between'>
-      <Link className='m-6 font-courier text-[30px]' href="/">
-          Anouk Desury
+      <Link className='m-6 lg:mb-16 font-courier text-[30px]' href="/">
+          <div className='lg:fixed'>Anouk Desury</div>
         </Link>
         <div className='block lg:hidden'><SmMenu projects={projects}/></div>
     </div>
