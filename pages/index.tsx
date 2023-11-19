aede652c910d11aa7c24b0f172dcc114c309a020
IPreferWater <ipreferwater2@gmail.com>
2023-11-19 11:33:41 +0100

    feat(init): remove useless imgs & object-contain on img index

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ type IndexProps = {
   return (<Layout title="Anouk Desury photographe Roubaix" metaName = "Acceuil" metaDescription="Page d'acceuil d' Anouk Desury photgraphe Roubaix" projects={projects}>
 
 <div>
-  <img className='lg:flex lg:ml-56 h-screen lg:h-[600px]' src='/projects/les_poings_ouverts/Les poings ouverts Anouk Desury_190.webp' alt='photo de profil d Anouck Desury' />
+  <img className='object-contain lg:flex lg:ml-56 h-screen lg:h-[600px]' src='/projects/les_poings_ouverts/Les poings ouverts Anouk Desury_190.webp' alt='photo de profil d Anouck Desury' />
 </div>
   </Layout>)
 }
