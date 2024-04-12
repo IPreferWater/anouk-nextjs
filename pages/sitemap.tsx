c33bccba22a4409a7482b80b809d24a0c8727e00
IPreferWater <51187329+IPreferWater@users.noreply.github.com>
2024-04-12 14:57:27 +0200

    1.2.2
    fix error in sitemap.xml at "priority"

--- pages/sitemap.tsx
+++ pages/sitemap.tsx
@@ -1,6 +1,5 @@
-import { Layout } from '@/components/Layout'
-import { IProject, IProjectIndexLeftMenu } from '@/interfaces/index'
-import { getAllProjectsIds, getAllProjectsTitleSortedByDate, getProjectByID } from '@/api/project'
+import { IProject } from '@/interfaces/index'
+import { getAllProjectsIds, getProjectByID } from '@/api/project'
 
 type SitemapProps = {
   projects: IProject[]
@@ -18,7 +17,6 @@ export default function Sitemap({ projects }: SitemapProps) {
         <loc>https://www.anoukdesury.com</loc>
         <lastmod>2024-01-01</lastmod>
         <changefreq>yearly</changefreq>
-        <priority> </priority>
       </url>
       <url>
         <loc>https://www.anoukdesury.com/presse</loc>
